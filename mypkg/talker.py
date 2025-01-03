63933e775b3eac811a5bc5adb06fa57b5d4026c2
Shuta Tsukui <tashu4649@gmail.com>
2025-01-03 20:42:18 +0900

    mypkg

---
+++ mypkg/talker.py
@@ -0,0 +1,22 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.msg import Person
+
+rclpy.init()
+node = Node("talker")
+pub = node.create_publisher(Person, "person", 10)
+n = 0
+
+
+def cb():
+    global n
+    msg = Person()
+    msg.name = "筑井崇太"
+    msg.age = n
+    pub.publish(msg)
+    n += 1
+
+
+def main():
+    node.create_timer(0.5, cb)
+    rclpy.spin(node)
