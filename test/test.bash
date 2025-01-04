bcc7da58b412f8b09954f91a939afee0c75f9909
Shuta Tsukui <tashu4649@gmail.com>
2025-01-05 04:26:27 +0900

    ten com

--- test/test.bash
+++ test/test.bash
@@ -2,23 +2,15 @@
 # SPDX-FileCopyrightText: 2025 Tsukui Shuta
 # SPDX-License-Identifier: BSD-3-Clause
 
-dir=~
-[ "$1" != "" ] && dir="$1"
 
-cd $dir/ros2_ws || { echo "Failed to change directory to $dir/ros2_ws"; exit 1; }
+dir=~                      
+[ "$1" != "" ] && dir="$1" 
 
-colcon build || { echo "colcon build failed"; exit 1; }
-source $dir/.bashrc
+cd $dir/ros2_ws 
+colcon build      
+source $dir/.bashrc        
 
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log || { echo "ros2 launch failed"; exit 1; }
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log || { echo "Failed to read log file"; exit 1; }
-
-#mojiretu kensaku
-if grep -q 'Received fish info' /tmp/mypkg.log; then
-    echo "Success: 'Received fish info' found in /tmp/mypkg.log"
-    exit 0
-else
-    echo "Error: 'Received fish info' not found in /tmp/mypkg.log"
-    exit 1
-fi
+cat /tmp/mypkg.log |
+grep 'Received fish info:'
